da1feada3e571127029e380ad9497d91472b82e1
Tomoya Fukushima <218260@st.takushoku-u.ac.jp>
2023-11-30 22:29:35 +0900

    第10回講義時点での進捗

---
+++ test copy.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <cstdlib>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Input image is required.\n");
+    return EXIT_FAILURE;
+  }
+
+  // 画像の読み込み
+  cv::Mat img = cv::imread(argv[1], cv::IMREAD_ANYCOLOR);
+  if (img.empty()) {  // 画像ファイルが見つからない場合の処理
+    printf("Input image is not found.\n");
+    return EXIT_FAILURE;
+  }
+  for (int y = 0; y < img.rows; y++) {
+    for (int x = 0; x < img.cols; x++) {
+      int idx = y * img.cols + x;
+      img.data[idx] = 255 - img.data[idx];
+    }
+  }
+
+  // 画像の表示
+  cv::imshow("window", img);
+  // キー入力を待つ
+  cv::waitKey();
+  // 全てのウインドウを破棄
+  cv::destroyAllWindows();
+
+  return EXIT_SUCCESS;
+}
